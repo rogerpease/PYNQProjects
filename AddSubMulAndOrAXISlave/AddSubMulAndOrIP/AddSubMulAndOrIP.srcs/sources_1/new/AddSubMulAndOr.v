2ec5a418a0d723fc3a29c3ea5638f9adc46c8dca
Roger Pease <rogerpease@gmail.com>
2020-06-14 19:45:57 -0500

    Widened Product.

--- AddSubMulAndOrAXISlave/AddSubMulAndOrIP/AddSubMulAndOrIP.srcs/sources_1/new/AddSubMulAndOr.v
+++ AddSubMulAndOrAXISlave/AddSubMulAndOrIP/AddSubMulAndOrIP.srcs/sources_1/new/AddSubMulAndOr.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Roger D. Pease
 // 
 // Create Date: 06/14/2020 06:10:29 PM
-// Design Name: 
+// Design Name: Add Subtract Multiply And Or function 
 // Module Name: AddSubMulAndOr
 // Project Name: 
-// Target Devices: 
+// Target Devices: Zynq 7000 
 // Tool Versions: 
 // Description: 
 // 
@@ -25,13 +25,17 @@ module AddSubMulAndOr(
     input [31:0] b,
     output [31:0] sum,
     output [31:0] difference,
-    output [31:0] product,
+    output [31:0] productLSB,
+    output [31:0] productMSB,
     output [31:0] bitwiseAnd,
     output [31:0] bitwiseOr
     );
+    wire [63:0] product;
     assign sum = a+b;
     assign difference = a-b;
     assign product = a*b;
+    assign productLSB = product[31:0];
+    assign productMSB = product[63:32];
     assign bitwiseAnd = a & b;
     assign bitwiseOr  = a | b; 
 endmodule
--- AddSubMulAndOrAXISlave/AddSubMulAndOrIP/AddSubMulAndOrIP.srcs/sources_1/new/tb_AddSubMulAndOr.v
+++ AddSubMulAndOrAXISlave/AddSubMulAndOrIP/AddSubMulAndOrIP.srcs/sources_1/new/tb_AddSubMulAndOr.v
@@ -4,7 +4,9 @@ module tb_AddSubMulAndOr();
    reg [31:0] b; 
    wire [31:0] sum;
    wire [31:0] difference;
-   wire [31:0] product; 
+   wire [31:0] productLSB; 
+   wire [31:0] productMSB; 
+   wire [63:0] product; 
    wire [31:0] bitwiseAnd;
    wire [31:0] bitwiseOr;
 
@@ -13,14 +15,20 @@ AddSubMulAndOr AddSubMulAndOr_inst(
     .b(b),
     .sum(sum),
     .difference(difference),
-    .product(product),
+    .productLSB(productLSB),
+    .productMSB(productMSB),
     .bitwiseAnd(bitwiseAnd),
     .bitwiseOr(bitwiseOr)
     );
-      
+   assign product = {productMSB,productLSB} ;    
+
+  //
+  // Normally I would do a more comprehensive testbench but since this is an example I'm not really looking for a specific issue or corner. 
+  //
     
   initial begin : TESTBLOCK
    integer i,j; 
+   reg [63:0] expProduct; 
    for (i = 0; i < 32; i=i+1)
       for (j = 0; j < 32; j=j+1)
       begin  
@@ -29,16 +37,13 @@ AddSubMulAndOr AddSubMulAndOr_inst(
         #5
         if (sum != a + b) $stop("Sum Failed"); 
         if (difference != a - b) $stop("Difference Failed"); 
-        if (product != a * b) $stop("Multiplier Failed"); 
+	expProduct = a * b; 
+        if (product != expProduct) $stop("Multiplier Failed"); 
         if ((a & b) != bitwiseAnd) $stop("bitwiseAnd Failed"); 
         if ((a | b) != bitwiseOr)  $stop("bitwiseOr Failed"); 
       end
       $stop("Normal Termination");   
    end
-  
-  
-  
-  
   
   
 
