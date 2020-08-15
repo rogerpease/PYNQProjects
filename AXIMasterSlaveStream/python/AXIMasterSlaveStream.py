a6888e6360e0e40288e5dde2749be831bbb932b6
Roger Pease <rogerpease@gmail.com>
2020-08-15 09:29:18 -0500

    Updated to format into test processes.

--- AXIMasterSlaveStream/python/AXIMasterSlaveStream.py
+++ AXIMasterSlaveStream/python/AXIMasterSlaveStream.py
@@ -1,57 +1,76 @@
 #!/usr/bin/env python3 
 
+import time 
 import sys
 from pynq import Overlay
 from pynq import allocate
 ol = Overlay("AXIMasterSlaveStream.bit") 
 
-input_buffer     = allocate(shape=(8,),dtype='u4')
-output_buffer    = allocate(shape=(8,),dtype='u4')
-input_buffer[:]  = [100,200,300,400,500,600,700,800]
-output_buffer[:] = [0,0,0,0,0,0,0,0]
-
 #
 # Send data to the peripheral. 
 #
-#
 dma = ol.axi_dma_0
-dma.sendchannel.transfer(input_buffer)
-dma.sendchannel.wait()
+
+def TestStreamSlave(input_buffer):
+
+  dma.sendchannel.start() 
+  dma.sendchannel.transfer(input_buffer)
+  dma.sendchannel.wait()
+  dma.sendchannel.stop() 
+
+  #
+  # Query the peripheral for the data we sent it. 
+  #
+
+
+  for i in range(8):
+    ol.AXIMasterSlaveStream_0.write(0x08,i) 
+    actual = ol.AXIMasterSlaveStream_0.read(0x0C)
+    if (actual != input_buffer[i]): 
+      print("Error on data from input buffer " + str(i) + \
+            " Actual " +str(actual) +                     \
+            " Expected " + str(input_buffer[i])) 
+      return 0 
+
+
 
 #
-# Query the peripheral for the data we sent it. 
-#
-
-
-for i in range(8):
-  ol.AXIMasterSlaveStream_0.write(0x08,i) 
-  actual = ol.AXIMasterSlaveStream_0.read(0x0C)
-  if (actual != input_buffer[i]): 
-    print("Error on data from input buffer " + str(i) + \
-          " Actual " +str(actual) +                     \
-          " Expected " + str(input_buffer[i])) 
-    sys.exit(1) 
-#
-# Now stream back 8 words of data. 
+# Set the Master Stream to send us 8 words of data and set an output_buffer to receive them. 
 #
 #
 
+def TestStreamMaster(baseval):
 
-# Confiugre the peripheral to send 0xABCDFF00 .. 0xABCDFF07 
+  # Set DMA to write SS2M data to a specifc physical address 
+  output_buffer    = allocate(shape=(8,),dtype='u4')
+  output_buffer[:] = 0 
+  dma.recvchannel.start() 
+  dma.recvchannel.transfer(output_buffer)
 
-ol.AXIMasterSlaveStream_0.write(0x04,0xABCDFF00) 
-ol.AXIMasterSlaveStream_0.write(0x00,0x01) 
+  # Confiugre the peripheral to send baseval to baseval + 7
+  ol.AXIMasterSlaveStream_0.write(0x00,0x02)
+  ol.AXIMasterSlaveStream_0.write(0x00,0x00) 
+  ol.AXIMasterSlaveStream_0.write(0x04,baseval)
+  ol.AXIMasterSlaveStream_0.write(0x00,0x01) 
+   
+  dma.recvchannel.wait()
+  dma.recvchannel.stop() 
 
-dma.recvchannel.transfer(output_buffer)
-dma.recvchannel.wait()
-
-for i in range(8):
-  expected = 0xABCDFF00 + i 
-  if not (output_buffer[i] == expected):
-    print("Error on output buffer " + str(i) +      \
+  # Check the data matches. 
+  for i in range(8):
+    expected = baseval + i 
+    if not (output_buffer[i] == expected):
+      print("TestStreamMaster: Error on output buffer " + str(i) +      \
           " Expected " + "{:x}".format(expected) +  \
           " Actual " + str(output_buffer[i])) 
-    sys.exit(1) 
+      return 0 
 
+input_buffer     = allocate(shape=(8,),dtype='u4')
+input_buffer[:]  = [x*100 for x in range(8)] 
+TestStreamSlave(input_buffer) 
+TestStreamMaster(0xABCDEF00) 
+input_buffer[:]  = [(x+8)*100 for x in range(8)] 
+TestStreamSlave(input_buffer) 
+TestStreamMaster(0xDECADE90)
 
 print ("PASS")
