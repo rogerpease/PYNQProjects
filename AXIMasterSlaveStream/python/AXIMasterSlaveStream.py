89004de9eb3f4e4ba1fe4b1b28245b2f71f9b3fc
Roger Pease <rogerpease@gmail.com>
2020-08-15 09:36:08 -0500

    Minor touchups.

--- AXIMasterSlaveStream/python/AXIMasterSlaveStream.py
+++ AXIMasterSlaveStream/python/AXIMasterSlaveStream.py
@@ -13,6 +13,8 @@ dma = ol.axi_dma_0
 
 def TestStreamSlave(input_buffer):
 
+  # Send MM2S data from CPU Memory to Stream by DMA. 
+
   dma.sendchannel.start() 
   dma.sendchannel.transfer(input_buffer)
   dma.sendchannel.wait()
@@ -21,17 +23,15 @@ def TestStreamSlave(input_buffer):
   #
   # Query the peripheral for the data we sent it. 
   #
-
-
   for i in range(8):
     ol.AXIMasterSlaveStream_0.write(0x08,i) 
     actual = ol.AXIMasterSlaveStream_0.read(0x0C)
-    if (actual != input_buffer[i]): 
+    if not (actual == input_buffer[i]): 
       print("Error on data from input buffer " + str(i) + \
             " Actual " +str(actual) +                     \
             " Expected " + str(input_buffer[i])) 
       return 0 
-
+  return 1 
 
 
 #
@@ -64,13 +64,19 @@ def TestStreamMaster(baseval):
           " Expected " + "{:x}".format(expected) +  \
           " Actual " + str(output_buffer[i])) 
       return 0 
+  return 1 
 
-input_buffer     = allocate(shape=(8,),dtype='u4')
-input_buffer[:]  = [x*100 for x in range(8)] 
-TestStreamSlave(input_buffer) 
-TestStreamMaster(0xABCDEF00) 
-input_buffer[:]  = [(x+8)*100 for x in range(8)] 
-TestStreamSlave(input_buffer) 
-TestStreamMaster(0xDECADE90)
+#
+# Basic Test 
+#
 
+
+for iteration in range(20):
+  input_buffer     = allocate(shape=(8,),dtype='u4')
+  input_buffer[:]  = [(x*iteration)*100 for x in range(8)] 
+  passed = TestStreamSlave(input_buffer) or TestStreamMaster(0x04030201*iteration) 
+  if not passed: 
+    print ("FAILED iteration "+str(iteration)) 
+    sys.exit(0) 
+  
 print ("PASS")
