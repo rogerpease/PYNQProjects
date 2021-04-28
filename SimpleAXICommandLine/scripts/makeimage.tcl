11d2f4358b4e74231727be833a3340c0585af255
Roger Pease <rogerpease@gmail.com>
2021-04-28 07:37:36 -0500

    Removed hard paths.

--- SimpleAXICommandLine/scripts/makeimage.tcl
+++ SimpleAXICommandLine/scripts/makeimage.tcl
@@ -1,8 +1,8 @@
-create_project FPGAImage /home/rpease/PYNQProjects/SimpleAXICommandLine/FPGAImage -part xc7z020clg400-1
+create_project FPGAImage ./FPGAImage -part xc7z020clg400-1
 set_property board_part tul.com.tw:pynq-z2:part0:1.0 [current_project]
 create_bd_design "block_design_1"
 update_compile_order -fileset sources_1
-set_property  ip_repo_paths  /home/rpease/PYNQProjects/SimpleAXICommandLine/verilog/module [current_project]
+set_property  ip_repo_paths  ./verilog/module [current_project]
 update_ip_catalog
 startgroup
 create_bd_cell -type ip -vlnv user.org:user:myip_v1_0:1.0 myip_v1_0_0
@@ -12,7 +12,7 @@ create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_sy
 endgroup
 apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external "FIXED_IO, DDR" apply_board_preset "1" Master "Disable" Slave "Disable" }  [get_bd_cells processing_system7_0]
 apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {Auto} Clk_slave {Auto} Clk_xbar {Auto} Master {/processing_system7_0/M_AXI_GP0} Slave {/myip_v1_0_0/s00_axi} ddr_seg {Auto} intc_ip {New AXI Interconnect} master_apm {0}}  [get_bd_intf_pins myip_v1_0_0/s00_axi]
-make_wrapper -files [get_files /home/rpease/PYNQProjects/SimpleAXICommandLine/FPGAImage/FPGAImage.srcs/sources_1/bd/block_design_1/block_design_1.bd] -top
-add_files -norecurse /home/rpease/PYNQProjects/SimpleAXICommandLine/FPGAImage/FPGAImage.gen/sources_1/bd/block_design_1/hdl/block_design_1_wrapper.v
+make_wrapper -files [get_files ./FPGAImage/FPGAImage.srcs/sources_1/bd/block_design_1/block_design_1.bd] -top
+add_files -norecurse ./FPGAImage/FPGAImage.gen/sources_1/bd/block_design_1/hdl/block_design_1_wrapper.v
 launch_runs impl_1 -to_step write_bitstream -jobs 2
 wait_on_run impl_1
--- SimpleAXICommandLine/scripts/packageip.tcl
+++ SimpleAXICommandLine/scripts/packageip.tcl
@@ -1,30 +1,19 @@
-#-----------------------------------------------------------
-# Vivado v2020.2 (64-bit)
-# SW Build 3064766 on Wed Nov 18 09:12:47 MST 2020
-# IP Build 3064653 on Wed Nov 18 14:17:31 MST 2020
-# Start of session at: Wed Apr 28 00:04:28 2021
-# Process ID: 23339
-# Current directory: /home/rpease/PYNQProjects/SimpleAXICommandLine
-# Command line: vivado
-# Log file: /home/rpease/PYNQProjects/SimpleAXICommandLine/vivado.log
-# Journal file: /home/rpease/PYNQProjects/SimpleAXICommandLine/vivado.jou
-#-----------------------------------------------------------
-create_project project_1 /home/rpease/PYNQProjects/SimpleAXICommandLine/project_1 -part xc7vx485tffg1157-1
-ipx::infer_core -vendor user.org -library user -taxonomy /UserIP /home/rpease/PYNQProjects/SimpleAXICommandLine/verilog/module
-ipx::edit_ip_in_project -upgrade true -name edit_ip_project -directory /home/rpease/PYNQProjects/SimpleAXICommandLine/project_1/project_1.tmp /home/rpease/PYNQProjects/SimpleAXICommandLine/verilog/module/component.xml
-ipx::current_core /home/rpease/PYNQProjects/SimpleAXICommandLine/verilog/module/component.xml
+create_project project_1 ./project_1 -part xc7vx485tffg1157-1
+ipx::infer_core -vendor user.org -library user -taxonomy /UserIP ./verilog/module
+ipx::edit_ip_in_project -upgrade true -name edit_ip_project -directory ./project_1/project_1.tmp ./verilog/module/component.xml
+ipx::current_core ./verilog/module/component.xml
 update_compile_order -fileset sources_1
 close_project
-ipx::infer_core -vendor user.org -library user -taxonomy /UserIP /home/rpease/PYNQProjects/SimpleAXICommandLine/verilog/module
-ipx::edit_ip_in_project -upgrade true -name edit_ip_project -directory /home/rpease/PYNQProjects/SimpleAXICommandLine/project_1/project_1.tmp /home/rpease/PYNQProjects/SimpleAXICommandLine/verilog/module/component.xml
-ipx::current_core /home/rpease/PYNQProjects/SimpleAXICommandLine/verilog/module/component.xml
+ipx::infer_core -vendor user.org -library user -taxonomy /UserIP ./verilog/module
+ipx::edit_ip_in_project -upgrade true -name edit_ip_project -directory ./project_1/project_1.tmp ./verilog/module/component.xml
+ipx::current_core ./verilog/module/component.xml
 update_compile_order -fileset sources_1
-ipx::package_project -root_dir /home/rpease/PYNQProjects/SimpleAXICommandLine/verilog/module -vendor user.org -library user -taxonomy /UserIP -force
+ipx::package_project -root_dir ./verilog/module -vendor user.org -library user -taxonomy /UserIP -force
 ipx::merge_project_changes hdl_parameters [ipx::current_core]
 set_property core_revision 2 [ipx::current_core]
 ipx::create_xgui_files [ipx::current_core]
 ipx::update_checksums [ipx::current_core]
 ipx::check_integrity [ipx::current_core]
 ipx::save_core [ipx::current_core]
-set_property  ip_repo_paths  /home/rpease/PYNQProjects/SimpleAXICommandLine/verilog/module [current_project]
+set_property  ip_repo_paths  ./verilog/module [current_project]
 update_ip_catalog
