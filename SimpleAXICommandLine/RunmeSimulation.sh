6eec026ee68404452b5533a385858564dbf3dbc7
Roger Pease <rogerpease@gmail.com>
2021-04-27 20:50:00 -0500

    Initial Revision.

--- SimpleAXICommandLine/RunmeSimulation.sh
+++ SimpleAXICommandLine/RunmeSimulation.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
+#
+# NOTE: This script is my own, but most of the verilog code is from auto-generated from an AXI Peripheral TB which I adapted to my needs. 
+# NOTE: Normally I'd do this with a makefile but this is more for notes/education purposes so drawing out the steps makes more sense. 
+#
+# Quick script to capture how to compile verilog for simulation on the command line with Vivado. 
+#
+#
+
+
 export LD_LIBRARY_PATH=/tools/Xilinx/.xinstall/Vivado_2020.2/lib/lnx64.o:/tools/Xilinx/Vivado/2020.2/lib/lnx64.o:/tools/Xilinx/.xinstall/Vivado_2020.2/lib/lnx64.o/SuSE/
 
 export VIVADO_BIN=/tools/Xilinx/Vivado/2020.2/bin
 
-#/tools/Xilinx/Vivado/2020.2/bin/vivado -mode batch -source Throwaway.tcl
 
 #
-# Make sure to use "-L uva -L axi_vip_v1_1_8 -L xilinx_vip" for all the axi libraries and 
-#   --sv for systemverilog
+# Make sure to use 
+#    "-L uva -L axi_vip_v1_1_8 -L xilinx_vip" for all the axi libraries and 
+#    --sv for systemverilog
+#
+# Don't confuse 
 #
 
 
