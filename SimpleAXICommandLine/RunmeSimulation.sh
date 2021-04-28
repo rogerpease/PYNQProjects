7ad61716bf6da767ff80d70975d0094ac1e2763e
Roger Pease <rogerpease@gmail.com>
2021-04-27 23:08:24 -0500

    Moved files.

--- SimpleAXICommandLine/RunmeSimulation.sh
+++ SimpleAXICommandLine/RunmeSimulation.sh
@@ -26,33 +26,33 @@ rm -rf xsim.dir
 $VIVADO_BIN/xvlog -work xil_defaultlib \
    --include "./verilog/include"  \
    --include "/tools/Xilinx/Vivado/2020.2/data/xilinx_vip/include" \
-    "verilog/myip_v1_0_S00_AXI.v" \
-    "verilog/myip_v1_0.v" \
-    "verilog/myip_v1_0_bfm_1_myip_0_0.v" 
+    "verilog/module/myip_v1_0_S00_AXI.v" \
+    "verilog/module/myip_v1_0.v" \
+    "verilog/tb/myip_v1_0_bfm_1_myip_0_0.v" 
 
 
  
 $VIVADO_BIN/xvlog --sv -work xil_defaultlib -L uva -L axi_vip_v1_1_8 -L xilinx_vip \
   --include "./verilog/include"  \
   --include "/tools/Xilinx/Vivado/2020.2/data/xilinx_vip/include" \
-"verilog/myip_v1_0_bfm_1_master_0_0_pkg.sv" \
-"verilog/myip_v1_0_bfm_1_master_0_0.sv" 
+"verilog/tb/myip_v1_0_bfm_1_master_0_0_pkg.sv" \
+"verilog/tb/myip_v1_0_bfm_1_master_0_0.sv" 
 
 
 $VIVADO_BIN/xvlog -work xil_defaultlib -L uva -L axi_vip_v1_1_8 -L xilinx_vip \
     --include "./verilog/include"  \
     --include "/tools/Xilinx/Vivado/2020.2/data/xilinx_vip/include" \
-  "verilog/myip_v1_0_bfm_1.v" \
-  "verilog/myip_v1_0_bfm_1_wrapper.v" 
+  "verilog/tb/myip_v1_0_bfm_1.v" \
+  "verilog/tb/myip_v1_0_bfm_1_wrapper.v" 
 
 $VIVADO_BIN/xvlog --sv -work xil_defaultlib -L uva -L axi_vip_v1_1_8 -L xilinx_vip \
     --include "./verilog/include"  \
     --include "/tools/Xilinx/Vivado/2020.2/data/xilinx_vip/include" \
-  "verilog/myip_v1_0_tb.sv"
+  "verilog/tb/myip_v1_0_tb.sv"
 
 # compile glbl module
 
-$VIVADO_BIN/xvlog -work xil_defaultlib -L uva -L axi_vip_v1_1_8 -L xilinx_vip  verilog/glbl.v
+$VIVADO_BIN/xvlog -work xil_defaultlib -L uva -L axi_vip_v1_1_8 -L xilinx_vip  verilog/tb/glbl.v
 
 $VIVADO_BIN/xelab -wto fa0545d4865e4787811138bb88188e54 --incr --debug typical --relax --mt 8 -L xil_defaultlib -L axi_infrastructure_v1_1_0 -L axi_vip_v1_1_8 -L uvm -L xilinx_vip -L unisims_ver -L unimacro_ver -L secureip -L xpm --snapshot myip_v1_0_tb_behav xil_defaultlib.myip_v1_0_tb xil_defaultlib.glbl -log elaborate.log
 
