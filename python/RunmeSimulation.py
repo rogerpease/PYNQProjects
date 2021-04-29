9d6ca8a7b9de2d60d5d3d4a9075c357a856ae095
Roger Pease <rogerpease@gmail.com>
2021-04-29 10:32:47 -0500

    Initial Revision.

--- python/RunmeSimulation.py
+++ python/RunmeSimulation.py
@@ -7,23 +7,18 @@
 # Quick script to capture how to compile verilog for simulation on the command line with Vivado. 
 #
 #
+# TODO: Make this into a python object so that I can call 
+#
+#
 
 
 import os 
+import subprocess
+import json
 
 os.environ["LD_LIBRARY_PATH"]= "/tools/Xilinx/.xinstall/Vivado_2020.2/lib/lnx64.o:/tools/Xilinx/Vivado/2020.2/lib/lnx64.o:/tools/Xilinx/.xinstall/Vivado_2020.2/lib/lnx64.o/SuSE/"
 
-VIVADO_BIN = "/tools/Xilinx/Vivado/2020.2/bin"
-
-
-import json
-with open("config.json","r") as fp:
-  config = json.load(fp)
-
-TopLevelTB = config["TopLevelTB"] 
-
-if (TopLevelTB == ""):
-  print("TopLevelTB not found") 
+VIVADO_BIN = "/tools/Xilinx/Vivado/2020.2/bin/"
 
 
 
@@ -33,51 +28,91 @@ if (TopLevelTB == ""):
 #    --sv for systemverilog
 #
 
-os.system("rm -rf xsim.dir") 
 
 
+def GetVandSVFiles():
+  VerilogFiles = []
+  SVFiles      = []
+  for subdir in ["module","tb"]:
+    for file in os.listdir("Verilog/"+subdir):
+      if file.endswith(".v"):
+          VerilogFiles.append(os.path.join("Verilog/"+subdir, file))
+      if file.endswith(".sv"):
+          SVFiles.append(os.path.join("Verilog/"+subdir, file))
+  return VerilogFiles,SVFiles 
 
-VerilogFiles = []
-SVFiles      = []
-import os
-for subdir in ["module","tb"]:
-  for file in os.listdir("Verilog/"+subdir):
-    if file.endswith(".v"):
-        VerilogFiles.append(os.path.join("Verilog/"+subdir, file))
-    if file.endswith(".sv"):
-        SVFiles.append(os.path.join("Verilog/"+subdir, file))
 
-
-import subprocess
 def Run(commandString):
+  print("Running: "," ".join(commandString))
   result = subprocess.Popen(commandString)
   text = result.communicate()[0]
   return_code = result.returncode  
-  print(commandString, " Returning ",return_code)
+  print("Returning ",return_code)
+  if (return_code != 0):
+    exit(1)
+   
 
 
-print ("Running Verilog compile") 
-if (len(VerilogFiles)): 
-  command = [VIVADO_BIN + "/xvlog", "-work","xil_defaultlib", 
-     "--include","./Verilog/include","--include","/tools/Xilinx/Vivado/2020.2/data/xilinx_vip/include"]
+def RunVerilogCompile(VerilogFiles): 
+  print ("Running Verilog compile") 
+  if (len(VerilogFiles)): 
+    command = [VIVADO_BIN + "xvlog", "-work","xil_defaultlib", 
+       "--include","./Verilog/include","--include","/tools/Xilinx/Vivado/2020.2/data/xilinx_vip/include"]
   Run(command + VerilogFiles) 
 
  
-print ("Running SystemVerilog compile") 
-if (len(SVFiles)): 
-  command = [VIVADO_BIN + "/xvlog","--sv","-work","xil_defaultlib","-L","uva","-L","axi_vip_v1_1_8","-L","xilinx_vip", 
-    "--include", "./Verilog/include", "--include", "/tools/Xilinx/Vivado/2020.2/data/xilinx_vip/include"] 
-  Run(command + SVFiles) 
-else:
-  print ("No SV Files Found ") 
+def RunSVCompile(SVFiles): 
+  print ("Running SystemVerilog compile") 
+  if (len(SVFiles)): 
+    command = [VIVADO_BIN + "xvlog","--sv","-work","xil_defaultlib","-L","uva","-L","axi_vip_v1_1_8","-L","xilinx_vip", 
+      "--include", "./Verilog/include", "--include", "/tools/Xilinx/Vivado/2020.2/data/xilinx_vip/include"] 
+    Run(command + SVFiles) 
+  else:
+    print ("No SV Files Found ") 
 
 
-print ("Running xelab compile") 
-command = [VIVADO_BIN + "/xelab", "-wto","fa0545d4865e4787811138bb88188e54","--incr","--debug","typical","--relax",
-           "--mt","8","-L","xil_defaultlib","-L","axi_infrastructure_v1_1_0","-L","axi_vip_v1_1_8","-L","uvm","-L","xilinx_vip","-L","unisims_ver",
-           "-L","unimacro_ver","-L","secureip","-L","xpm","--snapshot",TopLevelTB,"xil_defaultlib."+TopLevelTB,"xil_defaultlib.glbl",
+def RunElab(topLevelTB,snapshot): 
+  print ("RunElab"," Top: ", topLevelTB," Snap: ", snapshot)
+  command = [VIVADO_BIN + "xelab", "-wto","fa0545d4865e4787811138bb88188e54","--incr","--debug","typical","--relax",
+           "--mt","8",
+           "-L","xil_defaultlib","-L","axi_infrastructure_v1_1_0","-L","axi_vip_v1_1_8","-L","uvm","-L","xilinx_vip","-L","unisims_ver",
+           "-L","unimacro_ver","-L","secureip","-L","xpm",
+           "--snapshot",snapshot,"xil_defaultlib."+topLevelTB,"xil_defaultlib.glbl",
            "-log","elaborate.log" ]
-Run(command) 
+  Run(command) 
 
+
+def RunSim(Snapshot): 
+  command = [VIVADO_BIN + "xsim", Snapshot,"--tclbatch","scripts/simulation."+Snapshot+".tcl"] 
+  Run(command) 
+
+if __name__ == "__main__":
+  with open("config.json","r") as fp:
+    config = json.load(fp)
+
+  try:
+    TopLevelTB = config["TopLevelTB"] 
+  except KeyError:
+    print("config TopLevelTB not found")
+    exit() 
+
+
+  try:
+    config["Snapshot"]  
+  except KeyError:
+    Snapshot = "default"
+  else: 
+    Snapshot = config["Snapshot"] 
+
+
+  os.system("rm -rf xsim.dir") 
+  VerilogFiles,SVFiles = GetVandSVFiles()
+  print ("\n".join(VerilogFiles)) 
+  print ("\n".join(SVFiles)) 
+  RunVerilogCompile(VerilogFiles)
+  RunSVCompile(SVFiles)
+  print (TopLevelTB,Snapshot) 
+  RunElab(TopLevelTB,Snapshot) 
+  RunSim(Snapshot) 
 
 
