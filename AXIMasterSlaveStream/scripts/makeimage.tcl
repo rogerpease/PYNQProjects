6e025955c78f7d60205c6ad0b34391e7a2512fec
Roger Pease <rogerpease@gmail.com>
2021-04-29 20:10:52 -0500

    Initial Revision.

--- AXIMasterSlaveStream/scripts/makeimage.tcl
+++ AXIMasterSlaveStream/scripts/makeimage.tcl
@@ -2,14 +2,13 @@
 # Vivado v2020.2 (64-bit)
 # SW Build 3064766 on Wed Nov 18 09:12:47 MST 2020
 # IP Build 3064653 on Wed Nov 18 14:17:31 MST 2020
-# Start of session at: Thu Apr 29 11:45:55 2021
-# Process ID: 8194
+# Start of session at: Thu Apr 29 19:35:47 2021
+# Process ID: 37481
 # Current directory: /home/rpease/PYNQProjects/AXIMasterSlaveStream
 # Command line: vivado
 # Log file: /home/rpease/PYNQProjects/AXIMasterSlaveStream/vivado.log
 # Journal file: /home/rpease/PYNQProjects/AXIMasterSlaveStream/vivado.jou
 #-----------------------------------------------------------
-
 create_project FPGAImage ./FPGAImage -part xc7z020clg400-1
 set_property board_part tul.com.tw:pynq-z2:part0:1.0 [current_project]
 set_property  ip_repo_paths  ./Verilog/module [current_project]
@@ -20,27 +19,33 @@ startgroup
 create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7_0
 endgroup
 startgroup
-create_bd_cell -type ip -vlnv rogerpease.com:rogerpease:AXIMasterSlaveStreamIP_v1_0:1.0 AXIMasterSlaveStream_0
-endgroup
-startgroup
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_dma:7.1 axi_dma_0
 endgroup
+set_property -dict [list CONFIG.c_include_sg {0} CONFIG.c_sg_include_stscntrl_strm {0} CONFIG.c_include_mm2s_dre {1} CONFIG.c_include_s2mm_dre {1}] [get_bd_cells axi_dma_0]
+set_property -dict [list CONFIG.PCW_USE_S_AXI_HP0 {1}] [get_bd_cells processing_system7_0]
+startgroup
+create_bd_cell -type ip -vlnv rogerpease.com:rogerpease:AXIMasterSlaveStreamIP_v1_0:1.0 AXIMasterSlaveStream_0
+endgroup
+connect_bd_intf_net [get_bd_intf_pins AXIMasterSlaveStream_0/m00_axis] [get_bd_intf_pins axi_dma_0/S_AXIS_S2MM]
+connect_bd_intf_net [get_bd_intf_pins axi_dma_0/M_AXIS_MM2S] [get_bd_intf_pins AXIMasterSlaveStream_0/s00_axis]
 apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external "FIXED_IO, DDR" apply_board_preset "1" Master "Disable" Slave "Disable" }  [get_bd_cells processing_system7_0]
 startgroup
 apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {Auto} Clk_slave {Auto} Clk_xbar {Auto} Master {/processing_system7_0/M_AXI_GP0} Slave {/axi_dma_0/S_AXI_LITE} ddr_seg {Auto} intc_ip {New AXI Interconnect} master_apm {0}}  [get_bd_intf_pins axi_dma_0/S_AXI_LITE]
+apply_bd_automation -rule xilinx.com:bd_rule:clkrst -config { Clk {/processing_system7_0/FCLK_CLK0 (100 MHz)} Freq {100} Ref_Clk0 {} Ref_Clk1 {} Ref_Clk2 {}}  [get_bd_pins AXIMasterSlaveStream_0/m00_axis_aclk]
 apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {Auto} Clk_slave {Auto} Clk_xbar {Auto} Master {/processing_system7_0/M_AXI_GP0} Slave {/AXIMasterSlaveStream_0/s00_axi} ddr_seg {Auto} intc_ip {New AXI Interconnect} master_apm {0}}  [get_bd_intf_pins AXIMasterSlaveStream_0/s00_axi]
+apply_bd_automation -rule xilinx.com:bd_rule:clkrst -config { Clk {/processing_system7_0/FCLK_CLK0 (100 MHz)} Freq {100} Ref_Clk0 {} Ref_Clk1 {} Ref_Clk2 {}}  [get_bd_pins AXIMasterSlaveStream_0/s00_axis_aclk]
 endgroup
-connect_bd_intf_net [get_bd_intf_pins axi_dma_0/M_AXIS_MM2S] [get_bd_intf_pins AXIMasterSlaveStream_0/s00_axis]
-connect_bd_intf_net [get_bd_intf_pins AXIMasterSlaveStream_0/m00_axis] [get_bd_intf_pins axi_dma_0/S_AXIS_S2MM]
-connect_bd_net [get_bd_pins axi_dma_0/m_axi_sg_aclk] [get_bd_pins processing_system7_0/FCLK_CLK0]
-connect_bd_net [get_bd_pins axi_dma_0/m_axi_mm2s_aclk] [get_bd_pins processing_system7_0/FCLK_CLK0]
-connect_bd_net [get_bd_pins axi_dma_0/m_axi_s2mm_aclk] [get_bd_pins processing_system7_0/FCLK_CLK0]
-connect_bd_net [get_bd_pins AXIMasterSlaveStream_0/s00_axis_aclk] [get_bd_pins processing_system7_0/FCLK_CLK0]
-connect_bd_net [get_bd_pins AXIMasterSlaveStream_0/m00_axis_aclk] [get_bd_pins processing_system7_0/FCLK_CLK0]
-connect_bd_net [get_bd_pins AXIMasterSlaveStream_0/m00_axis_aresetn] [get_bd_pins axi_dma_0/mm2s_prmry_reset_out_n]
-connect_bd_net [get_bd_pins axi_dma_0/s2mm_sts_reset_out_n] [get_bd_pins AXIMasterSlaveStream_0/s00_axis_aresetn]
+set_property location {3 900 -10} [get_bd_cells processing_system7_0]
+set_property location {3 900 -20} [get_bd_cells processing_system7_0]
+set_property location {3 900 -10} [get_bd_cells processing_system7_0]
+set_property location {3 900 -20} [get_bd_cells processing_system7_0]
+startgroup
+set_property -dict [list CONFIG.PCW_USE_S_AXI_HP0 {1}] [get_bd_cells processing_system7_0]
+endgroup
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/processing_system7_0/FCLK_CLK0 (100 MHz)} Clk_slave {Auto} Clk_xbar {Auto} Master {/axi_dma_0/M_AXI_MM2S} Slave {/processing_system7_0/S_AXI_HP0} ddr_seg {Auto} intc_ip {New AXI Interconnect} master_apm {0}}  [get_bd_intf_pins processing_system7_0/S_AXI_HP0]
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Clk_master {/processing_system7_0/FCLK_CLK0 (100 MHz)} Clk_slave {/processing_system7_0/FCLK_CLK0 (100 MHz)} Clk_xbar {/processing_system7_0/FCLK_CLK0 (100 MHz)} Master {/axi_dma_0/M_AXI_S2MM} Slave {/processing_system7_0/S_AXI_HP0} ddr_seg {Auto} intc_ip {/axi_mem_intercon} master_apm {0}}  [get_bd_intf_pins axi_dma_0/M_AXI_S2MM]
+save_bd_design
 make_wrapper -files [get_files ./FPGAImage/FPGAImage.srcs/sources_1/bd/design_1/design_1.bd] -top
 add_files -norecurse ./FPGAImage/FPGAImage.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
 launch_runs impl_1 -to_step write_bitstream -jobs 2
 wait_on_run impl_1
-open_run impl_1
