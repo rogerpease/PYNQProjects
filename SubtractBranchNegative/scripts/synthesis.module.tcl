392001cb8353b6cd07c07cd7616db819758ce366
Roger Pease <rogerpease@gmail.com>
2021-05-07 20:31:01 -0500

    Initial Revision

--- SubtractBranchNegative/scripts/synthesis.module.tcl
+++ SubtractBranchNegative/scripts/synthesis.module.tcl
@@ -1,11 +1,10 @@
-read_verilog [ glob ./Verilog/module/*.v ./Verilog/module/*.sv ]
-#read_xdc ./scripts/bft_full.xdc
+read_verilog [ glob ./Verilog/module/SBNDatapath.v ]
 # Run synthesis
-synth_design -top SBNModule -part xc7z020clg400-1 -flatten_hierarchy rebuilt
-create_clock -period 10.000 -name sys_clk_100mhz -waveform {0.000 5.000} [get_ports sbndatapath_aclk]
+synth_design -top SBNDatapath -part xc7z020clg400-1 -flatten_hierarchy rebuilt
+create_clock -period 10.000 -name sys_clk_100mhz -waveform {0.000 5.000} [get_ports clk]
 # Write design checkpoint
-write_checkpoint -force synthesis/post_synth
+write_checkpoint -force synthesis/module/post_synth
 # Write report utilization and timing estimates
-write_verilog -force synthesis/netlist.v
-report_utilization -force -file synthesis/utilization.txt
-report_timing -file synthesis/timing.t
+write_verilog -force synthesis/module/netlist.v
+report_utilization -force -file synthesis/module/utilization.txt
+report_timing -file synthesis/module/timing.t
