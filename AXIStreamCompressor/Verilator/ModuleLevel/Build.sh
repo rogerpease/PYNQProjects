a77040eb76ad5637bd161de05910b844eaa93542
Roger Pease <rogerpease@gmail.com>
2021-05-13 17:54:50 -0700

    Initial Revision.

---
+++ AXIStreamCompressor/Verilator/ModuleLevel/Build.sh
@@ -0,0 +1,7 @@
+verilator --cc --exe --trace --top-module AXIStreamCompressor --build -j TestModule.cpp ../../Verilog/module/AXIStreamCompressor.sv \
+                                                              ../../Verilog/module/CompressAndReturn.sv \
+                                                              ../../Verilog/module/CompressionModule.sv \
+                                                              ../../Verilog/module/ReturnFIFO.sv \
+                                                              ../../Verilog/module/StreamElement.sv -o TestModule
+
+#make -j -C obj_dir -f VStreamElement.mk VStreamElement 
