fa18ac358f8ed87bb49304dd4780400c45e72501
Roger Pease <rogerpease@gmail.com>
2021-05-20 14:10:48 -0500

    Fixed Issue where FrameGenerator puts out valid without valid data before ready.

--- FrameGenerator/Verilog/module/FrameGeneratorCore.sv
+++ FrameGenerator/Verilog/module/FrameGeneratorCore.sv
@@ -1,23 +1,26 @@
 module FrameGeneratorCore 
 #(
-   parameter DATAOUTWIDTHBYTES = 1
 )
 (
    input clk,
    input reset, 
 
-   output reg [DATAOUTWIDTHBYTES*8-1:0] dataOut,
+   output wire [7:0] dataOut,
    output reg dataOutLast,
    output reg dataOutValid,
    input  wire dataOutReady,
 
    input  wire [31:0] controlRegister,
+   input  wire [31:0] heightWidthRegister,
    input  wire [31:0] dataOutLastPeriod,
 
    output wire [15:0] CoreID,
    output wire [31:0] rowColCounter  
 
 );
+
+// Add width and height. Modulo operations are expensive, so we have
+
   // Control Register 0:   0 = enable, bit 1 = reset 
    wire controlReset;
    assign controlReset = controlRegister[1]; 
@@ -27,16 +30,20 @@ module FrameGeneratorCore
 
    parameter FORMAT = 0; 
    parameter NUMPIXELPLANES = 3; 
-   parameter HEIGHT         = 1080; 
-   parameter WIDTH          = 1920; 
-   parameter QUARTERWIDTH   = WIDTH/4; 
    parameter LastPeriod     = 256; 
 
+   wire [15:0] height; 
+   wire [15:0] width; 
+   assign height = heightWidthRegister[31:16]; 
+   assign width  = heightWidthRegister[15:0]; 
    
    reg [2:0] pixelPlane; 
    reg [12:0] rowCounter; 
    reg [12:0] colCounter; 
    reg [24:0] dataOutLastIndex; 
+
+  
+
 
    assign rowColCounter = { 3'b000,rowCounter,3'b000,colCounter }; 
 
@@ -47,7 +54,7 @@ module FrameGeneratorCore
          pixelPlane = 0; 
          rowCounter = 0; 
          colCounter = 0; 
-         dataOut      = 0; 
+//         dataOut           = 8'hff; 
          dataOutValid      <= 0; 
          dataOutLast       <= 0; 
          dataOutLastIndex  <= 0; 
@@ -55,53 +62,86 @@ module FrameGeneratorCore
        end 
      else  
        begin 
-         if ((dataOutReady == 1) && (controlEnable == 1))
-         begin 
-           if (FORMAT == 0) 
+         $display("MODULE: Toggle Clock DataOutReady ", dataOutReady, " Control Enable ",controlEnable); 
+         if (controlEnable == 0)
+           dataOutValid      <= 0; 
+         else 
+           dataOutValid      <= 1; 
+           if ((dataOutReady == 1) && (dataOutValid))
            begin 
-             if (dataOutLastIndex == dataOutLastPeriod[24:0]) 
+             if (FORMAT == 0) 
              begin 
-               dataOutLast <= 1; 
-               dataOutLastIndex <= 0; 
+               $display("MODULE: DataOutLastIndex: ",dataOutLastIndex, " dataOutLastPeriod ",dataOutLastPeriod); 
+               if (dataOutLastIndex == dataOutLastPeriod[24:0]) 
+               begin 
+                 $display("MODULE: Dataoutlast"); 
+                 dataOutLast <= 1; 
+                 dataOutLastIndex <= 0; 
+               end 
+               else 
+               begin 
+                 dataOutLast <= 0; 
+                 dataOutLastIndex <= dataOutLastIndex + 1; 
+               end 
+                  
+               $display("ColCounter: ",colCounter, " RowCounter ",rowCounter," pixelPlane ", pixelPlane," DataOut ",dataOut); 
+  
+/*     verilator lint_off WIDTH */ 
+               pixelPlane = (pixelPlane + 1); 
+               if (pixelPlane == NUMPIXELPLANES)
+               begin 
+                 pixelPlane = 0; 
+               end  
+               if (pixelPlane == 0) 
+               begin 
+                 colCounter = (colCounter + 1);
+                 if (colCounter == width[12:0])  
+                   colCounter = 0; 
+               end 
+
+               if ((colCounter == 0) && (pixelPlane == 0)) 
+                 rowCounter = (rowCounter + 1); 
+               if (rowCounter == height[12:0]) 
+                 rowCounter = 0;
+  
+ /* 
+               if (colCounter[12:0] < width[15:2]) 
+                 dataOut = (pixelPlane == 0) ? 8'hFF : 8'h00; // Blue 
+               else if (colCounter[11:0] < width[15:1]) 
+                 dataOut = (pixelPlane == 1) ? 8'hFF : 8'h00; // Red 
+               else if (colCounter[12:0] < (width[15:1]+width[15:2]))
+                 dataOut = (pixelPlane == 2) ? 8'hFF : 8'h00; // Green 
+               else
+                 dataOut = 8'hFF;                 // White. 
+ */
+/* verilator lint_on WIDTH */ 
+               
              end 
-             else 
-             begin 
-               dataOutLast <= 0; 
-               dataOutLastIndex <= dataOutLastIndex + 1; 
-             end 
-
-             if (colCounter < (QUARTERWIDTH)) 
-                dataOut = (pixelPlane == 0) ? 8'hFF : 8'h00; // Red 
-             else if (colCounter < (2*QUARTERWIDTH)) 
-                dataOut = (pixelPlane == 1) ? 8'hFF : 8'h00; // Green
-             else if (colCounter < (3*QUARTERWIDTH)) 
-                dataOut = (pixelPlane == 2) ? 8'hFF : 8'h00; // Blue 
-             else
-                dataOut = 8'hFF;                 // White. 
-
-             $display("ColCounter: ",colCounter, " RowCounter ",rowCounter," pixelPlane ", pixelPlane," DataOut ",dataOut); 
-
-             pixelPlane = (pixelPlane + 1) % NUMPIXELPLANES; 
-             if (pixelPlane == 0) 
-               colCounter = (colCounter + 1) % WIDTH;  
-             if ((colCounter == 0) && (pixelPlane == 0)) 
-               rowCounter = (rowCounter + 1) % HEIGHT;  
-           end 
+           end
            else 
            begin 
              $display(" FORMAT != 0 ", dataOutReady); 
            end
-           dataOutValid <= 1;
-         end 
-         else
-         begin  
-           $display("DataOutReady ", dataOutReady); 
-          // Do nothing  
-           dataOutValid <= 0;
-         end 
        end 
-
+     $display("MODULE: Ending Always "); 
    end 
+    
+ /* verilator lint_off WIDTH */
+
+   wire redArea; 
+   wire greenArea; 
+   wire blueArea ; 
+   wire whiteArea; 
+   assign redArea   = (colCounter < width[15:2]); 
+   assign greenArea = (!redArea) && (colCounter < width[15:1]); 
+   assign blueArea  = (!redArea) && (!greenArea) && (colCounter < (width[15:1]+width[15:2])); 
+   assign whiteArea = !redArea && ! greenArea && !blueArea; 
+
+   assign dataOut = ((pixelPlane == 0) && ((redArea) || (whiteArea))) ? 8'hFF : 
+                    ((pixelPlane == 1) && ((greenArea) || (whiteArea))) ? 8'hFF : 
+                    ((pixelPlane == 2) && ((blueArea) || (whiteArea))) ? 8'hFF : 8'h00;  
+
+
 
    assign CoreID = 16'h0DEB;
 
